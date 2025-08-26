7a2cd74c1c5302b093402cfb5fdef48aa75623c5
CHETAN1675 <mahajanchetan001@gmail.com>
2025-08-26 17:37:43 +0530

    Styling added

---
+++ Writing Complex JSX/components/ExpenseItem.jsx
@@ -0,0 +1,13 @@
+import "./ExpenseItem.css";
+function ExpenseItem() {
+  return(
+  <div className="expense-item">
+  <div>15 August 2023</div>
+      <div className="expense-item__location">Delhi</div>
+      <div className="expense-item__price">$10</div>
+    <div className="expense-item__description">Book</div>
+  </div >
+  )
+}
+
+export default ExpenseItem;
