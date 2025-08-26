8cb445fac56f34a13f9977be0a93336db38154db
CHETAN1675 <mahajanchetan001@gmail.com>
2025-08-26 17:57:19 +0530

    javascript inside Jsx

--- Writing Complex JSX/components/ExpenseItem.jsx
+++ Writing Complex JSX/components/ExpenseItem.jsx
@@ -1,11 +1,16 @@
 import "./ExpenseItem.css";
 function ExpenseItem() {
+  const expenseDate = new Date(23 ,7 ,15).toISOString();
+  const expenseLocation = "Bangalore"
+  const expensePrice = 50
+  const expenseTitle = "Insurance"
   return(
-  <div className="expense-item">
-  <div>15 August 2023</div>
-      <div className="expense-item__location">Delhi</div>
-      <div className="expense-item__price">$10</div>
-    <div className="expense-item__description">Book</div>
+
+      <div className="expense-item">
+      <div>{expenseDate}</div>
+      <div className="expense-item__location">{expenseLocation}</div>
+      <div className="expense-item__price">${expensePrice}</div>
+    <div className="expense-item__description">{expenseTitle}</div>
   </div >
   )
 }
