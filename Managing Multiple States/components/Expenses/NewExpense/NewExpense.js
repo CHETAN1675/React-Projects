6b4e2da3c1c3d5ec571d8416661511ce3e8724d4
CHETAN1675 <mahajanchetan001@gmail.com>
2025-09-02 22:04:24 +0530

    Render Expense Form Conditionally

--- Managing Multiple States/components/Expenses/NewExpense/NewExpense.js
+++ Managing Multiple States/components/Expenses/NewExpense/NewExpense.js
@@ -3,14 +3,32 @@ import ExpenseForm from "./ExpenseForm";
 import "./NewExpense.css";
 
 const NewExpense = (props) => {
+
+   const [isFormVisible, SetIsFormVisible] = useState(false);
+
+  const showFormHandler = () => {
+    SetIsFormVisible(true);
+  };
+  const hideFormHandler = () => {
+    SetIsFormVisible(false);
+}
+
   const saveExpenseDataHandler = (enteredExpenseData) => {
     const expenseData = {...enteredExpenseData,
       id: Math.random().toString()};
     props.onAddExpense(expenseData);
+    SetIsFormVisible(false);
   };
   return (
     <div className="new-expense">
-      <ExpenseForm onSaveExpenseData={onSaveExpenseDataHandler}/>
+       {!isFormVisible && (
+      <button onClick = {showFormHandler}>Add Expense</button>
+      )}
+      {isFormVisible && (
+      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler}
+      onCancel={hideFormHandler}
+      />
+      )}
     </div>
   );
 };
