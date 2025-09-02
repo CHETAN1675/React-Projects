76a4c69f8aa0d394f463aca23f300bfe27605429
CHETAN1675 <mahajanchetan001@gmail.com>
2025-09-02 14:57:54 +0530

    Lifitng the state up

--- Managing Multiple States/App.js
+++ Managing Multiple States/App.js
@@ -1,4 +1,6 @@
+import React from "react";
 import Expenses from "./components/Expenses/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense"
 
 function App() {
   const expenses = [
@@ -7,13 +9,16 @@ function App() {
     { id: 3, title: "Pen", date: new Date(2023, 2, 10), price: 1 },
     { id: 4, title: "Laptop", date: new Date(2023, 9, 17), price: 200 },
   ];
-
+const addExpenseHandler = (expense)=>{
+  console.log(expense);
+}
   return (
     <div>
-      <h2>Let's get Started</h2>
+      
+      <NewExpense onAddExpense = {addExpenseHandler} />
       <Expenses expenses={expenses} />
     </div>
   );
-}
+};
 
 export default App;
