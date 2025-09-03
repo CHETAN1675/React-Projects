039d3d057fa7dfd4ede706c0a2853966376167b8
CHETAN1675 <mahajanchetan001@gmail.com>
2025-09-03 19:03:19 +0530

    Use of High Order Component

---
+++ HOC/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import UserList from './UserList';
+import withLoader from './withLoader';
+
+const App = () => {
+  const list = ['Alice', 'Bob', 'Charlie'];
+  const EnhancedUserList = withLoader(UserList, list);
+
+  return (
+    <div>
+      <h1>Our Team Members</h1>
+      <EnhancedUserList />
+    </div>
+  );
+};
+
+export default App;
---
+++ HOC/withLoader.js
@@ -0,0 +1,25 @@
+import React, { useState, useEffect } from 'react';
+
+const withLoader = (WrappedComponent, data) => {
+//Write your code here and create your HOC 
+  //Just like in the video  
+  return function WithLoaderComponent(props) {
+    const [isLoading, setIsloading] = useState(true);
+    const [items, setItems] = useState([]);
+
+    useEffect(() => {
+      setTimeout(() => {
+        setItems(data);
+        setIsloading(false);
+      }, 2000);
+    }, []);
+
+    if (isLoading) {
+      return <div>Loading...</div>;
+    }
+    return <WrappedComponent items={items} />;
+  };
+
+};
+
+export default withLoader;
